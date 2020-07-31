014b2fd816cd9a23e033d3956b98d6a01e4530d0
Peter PJT <pjtsignups@gmail.com>
2020-07-31 09:14:58 -0700

    fix(HttpAction): store base and path so reconstructable from json

--- src/Action/HttpAction.ts
+++ src/Action/HttpAction.ts
@@ -4,10 +4,14 @@ export type Method = "GET"|"POST"
 export class HttpAction implements Action {
     method:Method
     url:string
+    base:string
+    path:string
     name:string
     variant="HttpAction"
     constructor({name,method,base,path}:{name:string,method:Method,base:string,path:string}){
         this.method = method
+        this.base = base
+        this.path = path
         this.url = base+path
         this.name = name
     }
