84d2da27692f53c77801a578522f92059711f87a
Peter PJT <pjtsignups@gmail.com>
2020-07-31 12:09:33 -0700

    fix(Macro): make macro actions wait for one before before run

--- src/Action/Macro.ts
+++ src/Action/Macro.ts
@@ -1,5 +1,11 @@
 import { Action, ActionSuccess } from "./Action";
 
+async function asyncForEach<T>(array:T[], callback:(item:T,index:number,array:T[])=>Promise<any>) {
+    for (let index = 0; index < array.length; index++) {
+        await callback(array[index], index, array);
+    }
+}
+
 export class Macro implements Action {
     actions:Action[]
     name:string
@@ -9,8 +15,9 @@ export class Macro implements Action {
         this.name = name
     }
     async run():Promise<ActionSuccess>{
+        await asyncForEach(this.actions,async (action:Action)=>await action.run())
         return {
-            successful:this.actions.every(async action=>(await action.run()).successful)
+            successful:true
         }
     }
 }
