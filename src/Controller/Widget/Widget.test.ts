9956b0122fc4e0b81e0c47b773b5f9b937c7c456
Peter PJT <pjtsignups@gmail.com>
2020-08-16 09:53:55 -0700

    test(createWidget): add createWidget test

--- src/Controller/Widget/Widget.test.ts
+++ src/Controller/Widget/Widget.test.ts
@@ -1,5 +1,5 @@
 import { describe,it } from "mocha"
-import { Widget } from "./Widget"
+import { Widget, createWidget } from "./Widget"
 import { expect } from "chai"
 import { ArrowLayout } from "./ArrowLayout"
 import { Blank } from "./Blank"
@@ -32,5 +32,27 @@ describe("Widget",()=>{
             run(new HLayout([new Blank()]))
             run(new VLayout([new Blank()]))
         })
+    })
+})
+
+describe("createWidget()",()=>{
+    it("should return the correct widget",()=>{
+        expect(createWidget(GridLayout,{width:1,height:1},new Blank())).to.deep.equal(new GridLayout({width:1,height:1},[
+            new Blank()
+        ]))
+        expect(createWidget(VLayout,undefined,new Blank())).to.deep.equal(new VLayout([new Blank()]))
+        expect(createWidget(ArrowLayout,{
+            left:new Blank(),
+            right:new Blank(),
+            center:new Blank(),
+            top:new Blank(),
+            bottom:new Blank(),
+        })).to.deep.equal(new ArrowLayout({
+            left:new Blank(),
+            right:new Blank(),
+            center:new Blank(),
+            top:new Blank(),
+            bottom:new Blank(),
+        }))
     })
 })
