32867066dc781438df978eb7645dfcc3309054f0
Peter PJT <pjtsignups@gmail.com>
2020-08-17 11:26:10 -0700

    docs(House,Room): add docs

--- src/House/House.ts
+++ src/House/House.ts
@@ -1,8 +1,43 @@
 import { Room } from "../Room";
 
+/**
+ * Houses are the highest level objects in the library. They hold an array of [[Room]]s
+ * @example
+ * ```jsx
+ * const device = new Device({
+ *       name:"testDevice",
+ *       actions:[
+ *           new HttpAction({
+ *               name:"on",
+ *               method:"GET",
+ *               base:"http://device.ip/",
+ *               path:"turnOn"
+ *           })
+ *       ]
+ * })
+ * 
+ * const controller = new Controller({
+ *     name:"Test Controller",
+ *     layout:[
+ *       <Button action={device.getAction("on")} icon="power-on"/>
+ *       //OR
+ *       //new Button({
+ *       //    action:device.getAction("on"),
+ *       //    icon:"power-on"
+ *       //})
+ *    ]
+ * })
+ * 
+ * const room = new Room({name:"Test Room",controllers:[controller]})
+ * new House({name:"Test House",rooms:[room]})
+ * ```
+ */
 export class House {
     name:string
     rooms:Room[]
+    /**
+     * @param name The display name of the House
+     */
     constructor({name,rooms}:{name:string,rooms:Room[]}){
         this.name = name
         this.rooms = rooms
--- src/Room/Room.ts
+++ src/Room/Room.ts
@@ -1,5 +1,36 @@
 import { Controller } from "../Controller";
 
+/**
+ * Rooms are a semantic organization unit for [[Controller]]s used in [[House]]s
+ * @example
+ * ```jsx
+ * const device = new Device({
+ *       name:"testDevice",
+ *       actions:[
+ *           new HttpAction({
+ *               name:"on",
+ *               method:"GET",
+ *               base:"http://device.ip/",
+ *               path:"turnOn"
+ *           })
+ *       ]
+ * })
+ * 
+ * const controller = new Controller({
+ *     name:"Test Controller",
+ *     layout:[
+ *       <Button action={device.getAction("on")} icon="power-on"/>
+ *       //OR
+ *       //new Button({
+ *       //    action:device.getAction("on"),
+ *       //    icon:"power-on"
+ *       //})
+ *    ]
+ * })
+ * 
+ * new Room({name:"Test Room",controllers:[controller]})
+ * ```
+ */
 export class Room {
     name:string
     controllers:Controller[]
