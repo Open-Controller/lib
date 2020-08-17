39291686f4a499f916bc3a4d4242a1443b5db14a
Peter PJT <pjtsignups@gmail.com>
2020-08-17 08:57:39 -0700

    docs(Controller): add docs

--- src/Controller/Controller.ts
+++ src/Controller/Controller.ts
@@ -1,8 +1,41 @@
 import { Widget } from "./Widget/Widget";
 
+/**
+ * Controllers are the main unit of the library. They store an array of widgets that are to be displayed together on a client.
+ * 
+ * @example
+ * ```typescript
+ * const device = new Device({
+ *       name:"testDevice",
+ *       actions:[
+ *           new HttpAction({
+ *               name:"on",
+ *               method:"GET",
+ *               base:"http://device.ip/",
+ *               path:"turnOn"
+ *           })
+ *       ]
+ * })
+ * 
+ * new Controller({
+ *     name:"Test Controller",
+ *     layout:[
+ *       new Button({
+ *           action:device.getAction("on"),
+ *           icon:"power-on"
+ *      })
+ *    ]
+ * })
+ * ```
+ */
 export class Controller {
     name:string
     layout:Widget[]
+    /**
+     * 
+     * @param name The name of the Controller displayed in the client
+     * @param layout An array of Widgets that will be displayed stacked or potentially wrapped on the client
+     */
     constructor({name,layout}:{name:string,layout:Widget[]}){
         this.name = name
         this.layout = layout
