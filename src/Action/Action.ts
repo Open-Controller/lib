3a5d7b918678903f6a2c76fca35b477e38d412a7
Peter PJT <pjtsignups@gmail.com>
2020-08-16 18:16:59 -0700

    docs(Action): add docs for Action

--- src/Action/Action.ts
+++ src/Action/Action.ts
@@ -1,17 +1,23 @@
 import { HttpAction, Macro, TelnetAction, DelayAction, TCPAction } from ".";
 
+/**Stores the success of an [[Action]] */
 export interface ActionSuccess {
     successful:boolean
 }
 
+/**Actions represent the calling of an external source without a return */
 export interface Action {
+    /**Metadata of the class name for parsing from a JSON */
     __variant__:string
+    /**A unique identifier for an action within a device */
     name:string
+    /**Executes the task, returning a promise containing its success */
     run():Promise<ActionSuccess>
 }
 
 export class Action {
-    static fromJSON(json:any){
+    /**Uses the __variant__ metadata to map a JSON object to a class */
+    static fromJSON(json:any):Action{
         switch (json.__variant__){
             case "HttpAction": 
                 return HttpAction.fromJSON(json);
--- src/Action/DelayAction.ts
+++ src/Action/DelayAction.ts
@@ -1,8 +1,19 @@
 import {Action,ActionSuccess} from "./Action"
+/**An Action to delay execution, primarily used for a [[Macro]] 
+ * 
+ * ### Example usage:
+ * 
+ * ```typescript
+ * new DelayAction({name:"wait",time:1000})
+ * ```
+*/
 export class DelayAction implements Action {
     time:number
     name:string
     __variant__="DelayAction"
+    /**
+     * @param time Time to delay for in milliseconds
+     */
     constructor({name,time}:{name:string,time:number}){
         this.name = name
         this.time = time
