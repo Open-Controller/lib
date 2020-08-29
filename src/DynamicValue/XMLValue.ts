46a5a3bbb2de7aaa651fd278fe8d9abf119c1cfb
PJTSearch <pjtsignups@gmail.com>
2020-08-28 19:10:43 -0700

    feat(XMLValue): switch to fast-xml-parser, add options param

--- src/DynamicValue/XMLValue.ts
+++ src/DynamicValue/XMLValue.ts
@@ -1,5 +1,5 @@
 import { DynamicValue, Listener, Unsubscriber } from "./DynamicValue"
-import { parseStringPromise as parseXML } from "xml2js"
+import { parse as parseXML, X2jOptionsOptional } from "fast-xml-parser"
 
 /**
  * A [[DynamicValue]] reducer returning the input XML parsed
@@ -15,22 +15,24 @@ export class XMLValue implements DynamicValue<object> {
     __variant__="XMLValue"
     input:DynamicValue<string>
     name:string|void
-    constructor(input:DynamicValue<string>,name?:string){
+    options:X2jOptionsOptional
+    constructor(input:DynamicValue<string>,options?: X2jOptionsOptional,name?:string){
         this.input = input
         this.name = name
+        this.options = options
     }
     onValue(listener:Listener<object>):Unsubscriber{
-        const unsubscribe = this.input.onValue(async val=> {
-            listener(await parseXML(val))
+        const unsubscribe = this.input.onValue(val=> {
+            listener(parseXML(val,this.options))
         })
         return ()=> {
             unsubscribe()
         }
     }
     async getValue(){
-        return await parseXML(await this.input.getValue())
+        return parseXML(await this.input.getValue(),this.options)
     }
-    static fromJSON(json: { input: any, name?:string }){
-        return new XMLValue(DynamicValue.fromJSON(json.input),json.name)
+    static fromJSON(json: { input: any, options?: X2jOptionsOptional,name?:string }){
+        return new XMLValue(DynamicValue.fromJSON(json.input),json.options,json.name)
     }
 }
